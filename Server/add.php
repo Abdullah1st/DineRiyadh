986a6ab7d503672edc5d10f80b4c55398f1cb756
vlad <vlad@github.com>
2024-08-24 19:52:57 -0700

    editing contact form reciever (php)

--- Server/add.php
+++ Server/add.php
@@ -1,7 +1,7 @@
 <?php
 //any connection without login form should forwarded to the form
-if ($_SERVER['REQUEST_METHOD'] != "POST"){
-    header('location:../html/frontend/login.php');
+if ($_SERVER['REQUEST_METHOD'] != "POST") {
+  header('location:../html/frontend/login.php');
 }
 
 if (($_COOKIE['userNAME'] and $_COOKIE['userPASS'])) {
@@ -9,47 +9,45 @@ if (($_COOKIE['userNAME'] and $_COOKIE['userPASS'])) {
 
 require_once "db_connection/connect.php";
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $itemName = $_POST["itemName"];
-  $itemDescription = $_POST["itemDescription"];
-  $itemLogo = $_FILES['itemLogo']['tmp_name'];
-  //encode image
-  $itemLogo = file_get_contents($itemLogo);
-  $itemLogo = $conn->real_escape_string($itemLogo);
-  
-
-  //ensure that the name is not exist
-  $query = "SELECT name FROM item WHERE name = '$itemName';";
-  $result = mysqli_query($conn, $query);
-  $isExist = mysqli_num_rows($result) > 0;
+$itemName = $_POST["itemName"];
+$itemDescription = $_POST["itemDescription"];
+$itemLogo = $_FILES['itemLogo']['tmp_name'];
+//encode image
+$itemLogo = file_get_contents($itemLogo);
+$itemLogo = $conn->real_escape_string($itemLogo);
 
 
+//ensure that the name is not exist
+$query = "SELECT name FROM item WHERE name = '$itemName';";
+$result = mysqli_query($conn, $query);
+$isExist = mysqli_num_rows($result) > 0;
 
-  //if not exist, inject the item
-  if (!($isExist )) {
-    $inserted_at = date('Y-m-d h:i:s A', time() - (3600 * 4));
-    $query = "INSERT INTO item (name, description, logo, inserted_at)
+
+
+//if not exist, inject the item
+if (!($isExist)) {
+  $inserted_at = date('Y-m-d h:i:s A', time() - (3600 * 4));
+  $query = "INSERT INTO item (name, description, logo, inserted_at)
       VALUES ('$itemName', '$itemDescription', '$itemLogo', '$inserted_at');";
-      try {
-        $result = mysqli_query($conn, $query);
-        echo "
+  try {
+    $result = mysqli_query($conn, $query);
+    echo "
           <h1 style=color:green;background-color:rgba(0,255,0,0.2);padding:20px>
           Item has been added successfully
           </h1>
           ";
-      } catch (mysqli_sql_exception) {
-        echo "
+  } catch (mysqli_sql_exception) {
+    echo "
         <h1 style=color:red;background-color:rgba(255,0,0,0.2);padding:20px>
         ERROR: item didn't inserted into database
         </h1><br>
         $conn->error";
-      }
-  } else {
-    echo"
+  }
+} else {
+  echo "
         <h1 style=color:red;background-color:rgba(255,0,0,0.2);padding:20px>
         Error: Item might be exist in the database
         </h1>";
-  }
 }
 ?>
 
--- Server/contact.php
+++ Server/contact.php
@@ -10,12 +10,40 @@ else header('location:../html/BackEnd/login.php');
 require_once "db_connection/connect.php";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    $username = $_POST["userName"];
-    $usermail = $_POST["userMail"];
-    $usermessage = $_POST["userMessage"];
+    $userName = filter_input(INPUT_POST, "userName", FILTER_SANITIZE_SPECIAL_CHARS);
+    $userMail = filter_input(INPUT_POST, "userMail", FILTER_SANITIZE_SPECIAL_CHARS);
+    $userMessage = filter_input(INPUT_POST, "userMessage", FILTER_SANITIZE_SPECIAL_CHARS);
 
-    $query = "INSERT INTO contact (name, email, message)
-    VALUES ('$username', '$usermail', '$usermessage');";
+  //ensure that the email is not exist
+  $query = "SELECT name FROM customersMessages WHERE email = '$userMail';";
+  $result = mysqli_query($conn, $query);
+  $isExist = mysqli_num_rows($result) > 0;
+
+  //if not exist, insert the message
+if (!($isExist)) {
+    $inserted_at = date('Y-m-d h:i:s A', time() - (3600 * 4));
+    $query = "INSERT INTO customersMessages 
+    VALUES ('id', '$userName', '$userMail', '$userMessage' , 1);";
+    try {
+      $result = mysqli_query($conn, $query);
+      echo "
+            <h1 style=color:green;background-color:rgba(0,255,0,0.2);padding:20px>
+            Item has been added successfully
+            </h1>
+            ";
+    } catch (mysqli_sql_exception) {
+      echo "
+          <h1 style=color:red;background-color:rgba(255,0,0,0.2);padding:20px>
+          ERROR: message hasn't been sent to us
+          </h1><br>
+          ";
+    }
+  } else {
+    echo "
+          <h1 style=color:red;background-color:rgba(255,0,0,0.2);padding:20px>
+          Error: Item might be exist in the database
+          </h1>";
+  }
 
     $result = mysqli_query($conn, $query);
     echo "<center style='font-size: 30px;margin-top: 300px;'><p>Thanks for rating</center>";
