08a1de45b3aff3804ead33cdb7a53619d4c47e13
vlad <vlad@github.com>
2024-08-30 09:38:47 -0700

    f

--- Server/login.php
+++ Server/login.php
@@ -7,27 +7,28 @@ if ($_SERVER['REQUEST_METHOD'] != "POST"){
 require_once "db_connection/connect.php";
 
 
-//  isset($_POST['login']) => less secure
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-
     //avoid SQL injection
-    $username1 = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
-    $password1 = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
-    $password1 = hash('md5', $password1, false);
-
-    $query = "SELECT username FROM administrators WHERE 
-    username = '$username1' and password = '$password1';";
-
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+    // $hashedPassword = hash('md5', $password, false);
+    // Down is another way of hashing
+    // $hashingPeriod = [
+    //     'cost' => 14
+    // ];
+    // $hashedPassword = password_hash($password, PASSWORD_BCRYPT, $hashingPeriod);
+    // bring the password from databast, then verify it ------------------------------
+    $query = "SELECT username FROM administrators WHERE
+    username = '$username' and password = '$password';";
+    //          using password_verify() function        ------------------------------
     $result = mysqli_query($conn, $query);
     if ($result) {
 
         if (mysqli_num_rows($result) > 0) {
             
     //name and password cookies for test purpose, TOKEN ID must be implemented
-            setcookie('userNAME', $username1, time() + 60 * 60 * 24, '/');
-            setcookie('userPASS', $password1, time() + 60 * 60 * 24, '/');
+            setcookie('userNAME', $username, time() + 60 * 60 * 24, '/');
+            setcookie('userPASS', $password, time() + 60 * 60 * 24, '/');
             header("location:../html/FrontEnd/admin.php");
         } else header("location:../html/FrontEnd/login.php");
         
     } else header("location:../html/FrontEnd/about.html");
-}
